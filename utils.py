30a7ebbde32608649e27c6cf3bd8c860cc3ac18a
sreevanijarugula <jsv1206@gmail.com>
2023-07-13 16:25:48 -0500

    writing a class for getting information from a table

--- utils.py
+++ utils.py
@@ -9,8 +9,32 @@ def Vizier(vo):
     k = int(np.nonzero(['obstap' not in rr['ivoid'] for rr in r])[0]) 
     return r[k].service
 
-def get_column_names(service, table_name):
-    q = " SELECT TOP 0 * "\
-        "FROM " + str(table_name) + ";"
-    df = service.search(q).to_table()
-    return df.colnames
+def to_table(service, q):
+    result = service.search(q).to_table()
+    return result
+
+class table_info():
+    def __init__(self, service, table_name):
+        self.service = service
+        self.table_name = table_name
+        
+    def get_column_names(self):
+        q = " SELECT TOP 0 * "\
+                "FROM " + str(self.table_name) + ";"
+        df = self.service.search(q).to_table()
+        return df.colnames
+        
+    def get_unique_values(self, column_name):
+        """
+        Get distinct values from a column in a table.
+        Input:
+            service: pyvo.dal.tap.TAPService
+            table_name: str
+            column_name: str
+        Output:
+            list
+        """
+        q = " SELECT DISTINCT " + str(column_name) + " "\
+            "FROM " + str(self.table_name) + ";"
+        df = self.service.search(q).to_table()
+        return df[column_name].tolist()
